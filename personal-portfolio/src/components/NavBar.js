96c30b75b39c1f638ce4fbd2d78134a2d6a870b4
scarhost <137666755+scarhost@users.noreply.github.com>
2023-10-15 21:04:19 -0700

    Update NavBar.js

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -1,66 +1,46 @@
 import { useState, useEffect } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
-import { HashLink } from 'react-router-hash-link';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import { Col, Row, Alert } from "react-bootstrap";
 
-export const NavBar = () => {
-
-  const [activeLink, setActiveLink] = useState('home');
-  const [scrolled, setScrolled] = useState(false);
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
 
   useEffect(() => {
-    const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    }
+    if (status === 'success') clearFields();
+  }, [status])
 
-    window.addEventListener("scroll", onScroll);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
 
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [])
-
-  const onUpdateActiveLink = (value) => {
-    setActiveLink(value);
+  const clearFields = () => {
+    setEmail('');
   }
 
   return (
-    <Router>
-      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-        <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>About me</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Shop my clothing</Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-              <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
-              </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink>
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </Router>
+      <Col lg={12}>
+        <div className="newsletter-bx wow slideInUp">
+          <Row>
+            <Col lg={12} md={6} xl={5}>
+              <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
+              {status === 'sending' && <Alert>Sent!</Alert>}
+              {status === 'error' && <Alert variant="danger">{message}</Alert>}
+              {status === 'success' && <Alert variant="success">{message}</Alert>}
+            </Col>
+            <Col md={6} xl={7}>
+              <form onSubmit={handleSubmit}>
+                <div className="new-email-bx">
+                  <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            </Col>
+          </Row>
+        </div>
+      </Col>
   )
 }
